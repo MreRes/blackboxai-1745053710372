c29afb1b7388d008ba4ef1ff6f5e3b791bfe9a1c
MreRes <47110975+MreRes@users.noreply.github.com>
2025-04-20 04:33:39 +0700

    Add tests/test_whatsapp_bot.py 1745098419577

--- tests/test_whatsapp_bot.py
+++ tests/test_whatsapp_bot.py
@@ -7,38 +7,38 @@ class TestWhatsAppBot(unittest.TestCase):
         self.storage = DataStorage(':memory:')  # Use in-memory DB for testing
 
     def test_add_transaction_income(self):
-        initial_report = self.storage.get_report()
+        initial_report = self.storage.get_report(1)
         initial_income = initial_report['total_income']
 
-        self.storage.add_transaction('income', 1000, 'Test income')
-        updated_report = self.storage.get_report()
+        self.storage.add_transaction(1, 'income', 1000, 'Test income')
+        updated_report = self.storage.get_report(1)
         self.assertEqual(updated_report['total_income'], initial_income + 1000)
 
     def test_add_transaction_outcome(self):
-        initial_report = self.storage.get_report()
+        initial_report = self.storage.get_report(1)
         initial_outcome = initial_report['total_outcome']
 
-        self.storage.add_transaction('outcome', 500, 'Test outcome')
-        updated_report = self.storage.get_report()
+        self.storage.add_transaction(1, 'outcome', 500, 'Test outcome')
+        updated_report = self.storage.get_report(1)
         self.assertEqual(updated_report['total_outcome'], initial_outcome + 500)
 
     def test_add_budget_and_get(self):
-        self.storage.add_budget('TestCategory', 2000, 'monthly')
-        budgets = self.storage.get_budgets()
+        self.storage.add_budget(1, 'TestCategory', 2000, 'monthly')
+        budgets = self.storage.get_budgets(1)
         self.assertTrue(any(b['category'] == 'TestCategory' and b['amount'] == 2000 for b in budgets))
 
     def test_add_goal_and_get(self):
-        self.storage.add_goal('TestGoal', 5000)
-        goals = self.storage.get_goals()
+        self.storage.add_goal(1, 'TestGoal', 5000)
+        goals = self.storage.get_goals(1)
         self.assertTrue(any(g['name'] == 'TestGoal' and g['target_amount'] == 5000 for g in goals))
 
     def test_update_goal_progress(self):
-        self.storage.add_goal('ProgressGoal', 10000)
-        goals = self.storage.get_goals()
+        self.storage.add_goal(1, 'ProgressGoal', 10000)
+        goals = self.storage.get_goals(1)
         goal = next((g for g in goals if g['name'] == 'ProgressGoal'), None)
         self.assertIsNotNone(goal)
         self.storage.update_goal_progress(goal['id'], 3000)
-        updated_goals = self.storage.get_goals()
+        updated_goals = self.storage.get_goals(1)
         updated_goal = next((g for g in updated_goals if g['id'] == goal['id']), None)
         self.assertEqual(updated_goal['current_amount'], 3000)
 
